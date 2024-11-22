2c778fda097e94eec51b5284e62241eb9c681dec
drewdoesdev <ataylor2247@gmail.com>
2024-11-22 14:18:56 -0600

    [ISSUE-11] Adding Obsidian to glob flow

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -88,7 +88,7 @@ export default function ListLayoutWithTags({
           <div>
             <ul>
               {displayPosts.map((post) => {
-                const { path, date, title, summary, tags, images, postImage } = post
+                const { path, date, title, summary, tags, postImageSrc, postImageAlt } = post
                 return (
                   <li key={path} className="py-5">
                     <article className="space-apart flex flex-col xl:flex-row">
@@ -98,8 +98,8 @@ export default function ListLayoutWithTags({
                       >
                         <Link href={`/${path}`}>
                           <Image
-                            src={postImage.src ?? '/static/images/goblin.png'}
-                            alt={postImage.alt ?? 'post image'}
+                            src={postImageSrc ?? '/static/images/goblin.png'}
+                            alt={postImageAlt ?? 'post image'}
                             width={1200}
                             height={350}
                             className="rounded-lg object-cover"
---
+++ templates/blog-post.md
@@ -0,0 +1,14 @@
+---
+title: "Post Title"
+date: YEAR-MM-DD
+tags:
+  - tag1
+  - tag2
+  - tag3
+draft: true
+postImageSrc: /path/to/image.jpg
+postImageAlt: Helpful alt text
+summary: Summary goes here.
+---
+
+Post content goes here.
